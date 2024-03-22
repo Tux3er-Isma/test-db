8831d07e23b82ce31386e5973a3a49c0832cb285
Tux3er <isma.tux3er@gmail.com>
2024-03-22 21:02:25 +0100

    Preparing for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
   output: "server",
-  adapter: netlify()
+  adapter: node({
+    mode: "standalone"
+  })
 });
